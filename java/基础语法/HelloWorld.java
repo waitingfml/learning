6142cb54bde92f6bb6fd438834883458b6ed08d3
waitingfml <waitingfml@outlook.com>
2023-05-10 20:24:20 +0800

    ✨ feat: 初始化

---
+++ java/基础语法/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    /*
+     * 第一个Java程序
+     * 它将输出字符串 Hello World
+     */
+    public static void main(String[] args) {
+        // 这是单行注释的示例
+        /* 这个也是单行注释的示例 */
+        System.out.println("Hello World"); // 输出 Hello World
+    }
+}
+
+// javac -encoding UTF-8 HelloWorld.java
+// java HelloWorld
